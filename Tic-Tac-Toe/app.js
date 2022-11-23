6a14bec1cf94dc2766e96ab28cf28106c743e40f
djszal <szalank.dj@gmail.com>
2022-11-23 15:44:22 -0500

    added player input and start button

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -23,7 +23,7 @@ function renderBoard() {
         const box = state.board[i];
         const cellElem = document.createElement('div');
         cellElem.classList.add('box');
-        cellElem.innerText = box; //check this. might not need since I wont have text to display
+        cellElem.innerText = 'box'; //check this. might not need since I wont have text to display@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         console.log(box);
         boardElem.appendChild(cellElem); 
         // if (i % 2 === 0){
@@ -33,15 +33,39 @@ function renderBoard() {
 }
 
 function renderPlayers() {
-    const playerElem = document.createElement('div')
-    const playerBox = state.players
+    const plyrInput1 = document.createElement('input');
+        plyrInput1.classList.add('player-input');
+        plyrInput1.value = 'Enter Player 1';
+        playerBoxElem.appendChild(plyrInput1)
+
+    const plyrInput2 = document.createElement('input');
+        plyrInput2.classList.add('player-input');
+        plyrInput2.value = 'Enter Player 2';
+        playerBoxElem.appendChild(plyrInput2);
+
+    const startButton = document.createElement('input');
+        startButton.classList.add('start-button');
+        startButton.value = 'Start';
+        startButton.type = 'button';
+        playerBoxElem.appendChild(startButton);
+
+
+    console.dir(plyrInput1)
+    
+
+
+    // let text;
+    // const plyr1 = document.createElement('div');
+    // text = `<input class="player-input" value="Enter Player 1"/> 
+    // <input class="player-input" value="Enter Player 2"/>
+    // <input class="start" value="Start Game" type="button"/>`;
 
 
 }
 
 buildInitialState()
 renderBoard()
-// renderPlayers()
+renderPlayers()
 
 
 //********************** Render the Board *****************************
